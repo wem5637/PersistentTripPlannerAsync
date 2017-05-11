a0443a0147540e79376bf9bca482ebdba1ede18d
Will McCracken <wem5637@gmail.com>
2017-05-10 23:18:27 -0500

    commit init for new repository using asynchronous ajax requests

---
+++ public/js/ajax.js
@@ -0,0 +1,45 @@
+var hotels;
+var restaurants;
+var activities;
+
+// application state
+
+
+
+var hotelPromise = Promise.resolve($.ajax({
+    method: 'GET',
+    url: '/api/hotels',
+
+}))
+
+
+var restPromise = Promise.resolve($.ajax({
+    method: 'GET',
+    url: '/api/restaurants',
+
+}))
+
+
+var activityPromise = Promise.resolve($.ajax({
+    method: 'GET',
+    url: '/api/activities',
+
+}))
+
+
+
+Promise.all([hotelPromise, restPromise, activityPromise])
+    .spread(function(hot, rest, activ) {
+        hotels = hot;
+        restaurants = rest;
+        activities = activ;
+
+        attractionsModule = attractionsModule();
+        assignOptions();
+    
+
+
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var Promise = require('bluebird');
+var router = require('express').Router();
+var Hotel = require('../models/hotel');
+var Restaurant = require('../models/restaurant');
+var Activity = require('../models/activity');
+
+
+router.get('/', function(req, res, next) {
+
+    res.render('index')
+});
+
+
+router.route('/api/hotels').get(function(req, res, next){
+  var arr;
+  Hotel.findAll()
+  .then(function(hotels){
+    arr = hotels;
+    res.json(arr);
+  })
+})
+
+router.route('/api/restaurants').get(function(req, res, next){
+  var arr;
+  Restaurant.findAll()
+  .then(function(restaurants){
+    arr = restaurants;
+    res.json(arr);
+  })
+})
+
+router.route('/api/activities').get(function(req, res, next){
+  var arr;
+  Activity.findAll()
+  .then(function(activities){
+    arr = activities;
+    res.json(arr);
+  })
+})
+
+module.exports = router;
